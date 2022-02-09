523f82edfe97fef70d0df433a890eb524921b9bf
kkjos <k19951227@gmail.com>
2022-02-10 00:56:13 +0900

    chapter3-practice3_3

---
+++ chapter3/src/chapter3/practice3_3.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class practice3_3 {
+	public static void main(String[] args) {
+		int isHungry = 1;
+		String food = "おにぎり";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹いっぱいです");
+		} else {
+			System.out.println("腹ペコです");
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
+}
