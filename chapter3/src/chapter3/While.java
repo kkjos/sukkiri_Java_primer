df7cc7f2544b8804d92884a4dd02e336b2707dbb
kkjos <k19951227@gmail.com>
2022-02-10 00:31:19 +0900

    chapter3-while

---
+++ chapter3/src/chapter3/While.java
@@ -0,0 +1,11 @@
+package chapter3;
+
+public class While {
+	public static void main(String[] args) {
+		boolean doorClose = true;
+		while (doorClose = true) {
+			System.out.println("ノックする");
+			System.out.println("1分まつ");
+		}
+	}
+}
