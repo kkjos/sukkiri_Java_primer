6762fd4f9ac9b342c38d54423008485b8529307e
kkjos <k19951227@gmail.com>
2022-02-09 12:50:17 +0900

    chapter1-DateType

---
+++ chapter1/src/dateType.java
@@ -0,0 +1,25 @@
+
+public class dateType {
+	public static void main(String[] args) {
+		byte watch = 2;
+		short age = 18;
+		int salary = 150000;
+		long worldPeople = 6000000000L;
+		
+		boolean isError = true;
+		boolean result = false;
+		
+		char zodiac = '虎';
+		String name = "すがわら";
+		
+		
+		System.out.println(watch);
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(worldPeople);
+		System.out.println(isError);
+		System.out.println(result);
+		System.out.println(zodiac);
+		System.out.println(name);
+	}
+}
