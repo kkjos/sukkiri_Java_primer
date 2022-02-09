79a9be9cbb57212e1f22a516ad4b4df0b5aad16c
kkjos <k19951227@gmail.com>
2022-02-09 11:37:58 +0900

    [finish]chapter0

---
+++ sukkiri_java_primer/src/chapter0/Helloworid.java
@@ -0,0 +1,7 @@
+package chapter0;
+
+public class Helloworid {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ sukkiri_java_primer/src/chapter0/Varible.java
@@ -0,0 +1,9 @@
+package chapter0;
+
+public class Varible {
+	public static void main(String[] args) {
+		int x;
+		x = 6;
+		System.out.println(x*x*3.14);
+	}
+}
