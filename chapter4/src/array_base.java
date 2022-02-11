7cab5151013d988442e68d78a4cfab27aa784b4c
kkjos <k19951227@gmail.com>
2022-02-11 13:10:03 +0900

    chapter4-arraybase

---
+++ chapter4/src/array_base.java
@@ -0,0 +1,14 @@
+
+public class array_base {
+	public static void main(String[] args) {
+
+		// 基礎
+		int[] scores = new int[5];
+
+		// 省略記法１
+		int[] scores1 = new int[] { 20, 30, 40, 50, 80 };
+
+		// 省略記法２
+		int[] scores2 = { 20, 30, 40, 50, 80 };
+	}
+}
