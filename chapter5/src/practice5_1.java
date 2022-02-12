9c258a20b7931069300a597283e2d060c2f669ee
kkjos <k19951227@gmail.com>
2022-02-12 12:17:14 +0900

    chapter5-practice5_1

---
+++ chapter5/src/practice5_1.java
@@ -0,0 +1,14 @@
+
+public class practice5_1 {
+	public static void introduceOneself() {
+		String name = "湊";
+		int age = 20;
+		double height = 179.5;
+		char zodiac = '虎';
+		System.out.println(name + "です。" + age + "歳で身長は" + height + "cm、" + zodiac + "年生まれです。");
+	}
+
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
