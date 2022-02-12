e51fc4a8ab65f69d85a8781d7274b257b5fa4a05
kkjos <k19951227@gmail.com>
2022-02-12 11:50:04 +0900

    chapter5-method03

---
+++ chapter5/src/method03.java
@@ -0,0 +1,12 @@
+
+public class method03 {
+	public static void main(String[] args) {
+		int ans = add(100, 200);
+		System.out.println(ans);
+	}
+
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+	}
+}
