9538aded6c20e7ee208e375a1323bf77579b4b83
kkjos <k19951227@gmail.com>
2022-02-12 12:28:37 +0900

    chapter5-practice5_3

---
+++ chapter5/src/practice5_3.java
@@ -0,0 +1,18 @@
+
+public class practice5_3 {
+	public static void email(String title, String address, String text) {
+		System.out.println(address + "に以下のメールを送信しました");
+		System.out.println("件名：" + title);
+		System.out.println("本文" + text);
+	}
+
+	public static void email(String address, String text) {
+		System.out.println(address + "に以下のメールを送信しました");
+		System.out.println("無題");
+		System.out.println("本文" + text);
+	}
+
+	public static void main(String[] args) {
+		email("xxx@xxx", "xxxの件よろしくお願いします。");
+	}
+}
