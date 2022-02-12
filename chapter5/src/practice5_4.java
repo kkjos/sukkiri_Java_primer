cee2058ed14863787fa5389b8f3e25d5addd5190
kkjos <k19951227@gmail.com>
2022-02-12 12:40:59 +0900

    chapter5-practice5_4

---
+++ chapter5/src/practice5_4.java
@@ -0,0 +1,20 @@
+
+public class practice5_4 {
+	public static double calcTriangleArea(double bottom, double height) {
+		double area = bottom * height / 2;
+		return area;
+	}
+
+	public static double calcCircleArea(double radius) {
+		double area = radius * radius * 3.14;
+		return area;
+	}
+
+	public static void main(String[] args) {
+		double triangleArea = calcTriangleArea(10.0, 5.0);
+		System.out.println("三角形の面積：" + triangleArea + "平方センチメートル");
+
+		double circleArea = calcCircleArea(5.0);
+		System.out.println("円の面積：" + circleArea + "平方センチメートル");
+	}
+}
