c349416645b3bdbfae070908478ddc4c3073bdb5
kkjos <k19951227@gmail.com>
2022-02-13 12:33:46 +0900

    chapter6-practice6_1

---
+++ chapter6/src/practice6/Main.java
@@ -0,0 +1,12 @@
+package practice6;
+
+import practice6.comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		// callDeae();
+		// showMondokoro();
+	}
+}
