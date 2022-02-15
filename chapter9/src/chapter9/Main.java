98b735e4a7b659cb9afe8448db7ac2dcb285eca0
kkjos <k19951227@gmail.com>
2022-02-15 11:52:06 +0900

    chapter9 class

--- chapter9/src/chapter9/Main.java
+++ chapter9/src/chapter9/Main.java
@@ -9,6 +9,18 @@ public class Main {
 		h.name = "ミナト";
 		h.hp = 100;
 		h.sword = s;
-		System.out.println("現在の武器は" + s.name);
+		System.out.println("現在の武器は" + h.sword.name);
+
+		Hero h1 = new Hero();
+		h1.name = "アサカ";
+		h1.hp = 100;
+		Hero h2 = new Hero();
+		h2.name = "スガワラ";
+		h2.hp = 50;
+		Wizard w = new Wizard();
+		w.name = "ミナト";
+		w.hp = 100;
+		w.heal(h1);
+		w.heal(h2);
 	}
 }
