cfc59e2c8a585f7f25859ad4aab012188a67fc43
kkjos <k19951227@gmail.com>
2022-02-15 11:42:00 +0900

    chapter9 has-a

--- chapter8/src/practice8/Cleric.java
+++ chapter8/src/practice8/Cleric.java
@@ -10,7 +10,19 @@ public class Cleric {
 	public void selfAid() {
 		System.out.println(this.name + "はセルフエイドを唱えた");
 		this.mp -= 5;
-		this.hp = MAX_HP;
+		this.hp = this.MAX_HP;
 		System.out.println("HPがマックスまで回復した");
+	}
+
+	public int pray(int sec) {
+		System.out.println(this.name + "は" + sec + "秒間に祈った");
+
+		int recover = new java.util.Random().nextInt(3) + sec;
+
+		int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+
+		this.mp += recoverActual;
+		System.out.println("MPが" + recoverActual + "回復した");
+		return recoverActual;
 	}
 }
