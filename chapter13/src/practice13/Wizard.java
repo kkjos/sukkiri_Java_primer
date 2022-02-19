e49d042e39ac067316b08a4c9ca474147d0c965d
kkjos <k19951227@gmail.com>
2022-02-19 12:43:50 +0900

    practice13_3

--- chapter13/src/practice13/Wizard.java
+++ chapter13/src/practice13/Wizard.java
@@ -1,14 +1,48 @@
 package practice13;
 
 public class Wizard {
-	int hp;
-	int mp;
-	String name;
-	Wand wand;
+	private int hp;
+	private int mp;
+	private String name;
+	private Wand wand;
+
 	void heal(Hero h) {
 		int basePoint = 10;
-		int recoverPoint =(int)(basePoint*this.wand.power);
-		h.setHp(h.getHp()+recoverPoint);
-		System.out.println(h.getName()+"のHPを"+recoverPoint+"回復した");
+		int recoverPoint = (int) (basePoint * this.getWand().getPower());
+		h.setHp(h.getHp() + recoverPoint);
+		System.out.println(h.getName() + "のHPを" + recoverPoint + "回復した");
 	}
+
+	public int getHp() {
+		return this.hp;
+	}
+
+	public void setHp(int Hp) {
+		this.hp = hp;
+	}
+
+	public int getMp() {
+		return this.mp;
+	}
+
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWand() {
+		return this.wand;
+	}
+
+	public void setWand(Wand wand) {
+		this.wand = wand;
+	}
+
 }
